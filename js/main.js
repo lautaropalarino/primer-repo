b517e4b9700369f9f7a9e7c811824d96771c50db
lautaropalarino <palarinolautaro@gmail.com>
2019-01-24 21:17:12 -0300

    Empezando el proyecto DX

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+- Mini Introduccion a JavaScript:
+Funciones y Eventos
+*/
+function saludar(){//<--- Operacion logica
+	window.alert("Hola desde Javascript")
+}
+
+document.querySelector("h1").onclick = saludar; //<-- Evento/Comportamiento...
